e8532793039bffca7a49caf2532f8463e1dc7e3a
Ansari33 <51727994+Ansari33@users.noreply.github.com>
2026-04-29 10:27:41 +0800

    Update LandingController.php

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -14,5 +14,45 @@ class LandingController extends Controller
         $konten = Konten::latest()->take(6)->get();
         return view('landing.beranda', compact('konten'));
     }
-    
+
+    public function sejarah()
+    {
+        return view('landing.sejarah');
+    }
+
+    public function visiMisi()
+    {
+        return view('landing.visi-misi');
+    }
+
+    public function tugasFungsi()
+    {
+        return view('landing.tugas-fungsi');
+    }
+
+    public function unitKerja()
+    {
+        return view('landing.unit-kerja');
+    }
+
+    public function berita()
+    {
+        return view('landing.berita');
+    }
+
+    public function dataKeagamaan()
+        {
+            return view('landing.data-keagamaan');
+        }
+
+    public function dataPendidikan()
+        {
+            return view('landing.data-pendidikan');
+        }   
+
+    public function manajemenPerubahan()
+        {
+            return view('landing.manajemen-perubahan');
+        }
+        
 }
